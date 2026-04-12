7302026bcd2ce914fec1e95a90c32e65db0ac096
Sol Buitron <maria.buitron.portilla@udla.edu.ec>
2026-04-11 22:53:34 -0600

    Maria Sol Buitron Tercer commit: Calcular y mostrar promedio de notas por estudiante

--- calificacion.c
+++ calificacion.c
@@ -1,7 +1,34 @@
-#include <stdio.h>
 
-int main(int argc, char const *argv[]) 
-{
-    int estudiantes = 5;
-    int asignaturas = 3;
-    float notas[5][3];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // Promedio por estudiante
+    printf("\nPromedio por Estudiante\n");
+    for (int i = 0; i < estudiantes; i++) {
+        float suma = 0;
+        for (int j = 0; j < asignaturas; j++)
+            suma += notas[i][j];
+        printf("Estudiante %d: %.2f\n", i + 1, suma / asignaturas);
+    }
+
+
+  return 0;
+
+}

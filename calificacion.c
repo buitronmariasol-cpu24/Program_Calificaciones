2357f9c5aae423e517ba34d3b237d0dd59448eec
buitronmariasol-cpu24 <maria.buitron.portilla@udla.edu.ec>
2026-04-11 22:07:54 -0600

    Correccion del margen

--- calificacion.c
+++ calificacion.c
@@ -1,39 +1,3 @@
-<<<<<<< HEAD
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    // Promedio por estudiante
-    printf("\nPromedio por Estudiante\n");
-    for (int i = 0; i < estudiantes; i++) {
-        float suma = 0;
-        for (int j = 0; j < asignaturas; j++)
-            suma += notas[i][j];
-        printf("Estudiante %d: %.2f\n", i + 1, suma / asignaturas);
-    }
-
-
-  return 0;
-
-}
-=======
 #include <stdio.h>
 
 int main(int argc, char const *argv[]) 
@@ -41,4 +5,10 @@ int main(int argc, char const *argv[])
     int estudiantes = 5;
     int asignaturas = 3;
     float notas[5][3];
->>>>>>> a4b87d560c6f67d3a73762c1b9307fdbff5ce6e6
+
+
+
+
+
+
+

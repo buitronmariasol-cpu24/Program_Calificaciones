940da7b9c9887682845651f1dbcbbbd4ca032061
buitronmariasol-cpu24 <maria.buitron.portilla@udla.edu.ec>
2026-04-11 22:16:00 -0600

    Tercer commit
    Calcular y mostrar promedio de notas por estudiante

--- calificacion.c
+++ calificacion.c
@@ -18,6 +18,17 @@ int main(int argc, char const *argv[])
                  }
     }
 
+// Promedio por estudiante
+//Maria Sol Buitron
+    printf("\nPromedio por Estudiante\n");
+    for (int i = 0; i < estudiantes; i++) {
+        float suma = 0;
+        for (int j = 0; j < asignaturas; j++)
+            suma += notas[i][j];
+        printf("Estudiante %d: %.2f\n", i + 1, suma / asignaturas);
+    }
+
+
 
 
 

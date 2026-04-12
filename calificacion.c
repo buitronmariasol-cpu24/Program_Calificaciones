6f0f21b0509bc21e51bf8a34e22f4afb4e025309
michellealt <michelle.altamirano@udla.edu.ec>
2026-04-11 23:13:57 -0500

    segundo commit
    Recorre cada estudiante, muestra su número, luego recorre cada asignatura, pide la nota, la guarda en la matriz y valida que esté entre 0 y 10, repitiendo el proceso hasta que el dato sea correcto.

--- calificacion.c
+++ calificacion.c
@@ -1,11 +1,22 @@
 #include <stdio.h>
-
 int main(int argc, char const *argv[]) 
 {
     int estudiantes = 5;
     int asignaturas = 3;
     float notas[5][3];
-
+// Ingresar calificaciones 
+// Michelle Altamirano
+    for (int i = 0; i < estudiantes; i++) {
+         printf("\nEstudiante %d\n", i + 1);
+          for (int j = 0; j < asignaturas; j++) {
+       do { 
+         printf("  Asignatura %d: ", j + 1); 
+          scanf("%f", &notas[i][j]); 
+           if (notas[i][j] < 0 || notas[i][j] > 10)
+           printf("  Error: ingresa un valor entre 0 y 10.\n");
+             } while (notas[i][j] < 0 || notas[i][j] > 10);
+                 }
+    }
 
 
 
